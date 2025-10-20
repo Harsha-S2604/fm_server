ebd06b3f32bd3db1b24914f5ee149821f80c5055
Harsha-S2604 <harsha91148@outlook.com>
2025-10-20 16:14:58 +0000

    feat: user_id Foreign key in files

--- setup_db.sql
+++ setup_db.sql
@@ -2,14 +2,26 @@ CREATE DATABASE IF NOT EXISTS fm_staging;
 USE fm_staging;
 
 DROP TABLE IF EXISTS files;
-CREATE TABLE IF NOT EXISTS files (
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users (
 	id BIGINT UNSIGNED PRIMARY KEY NOT NULL,
-	name varchar(200) NOT NULL,
-	location varchar(200) NOT NULL,
-	f_type varchar(200) NOT NULL
+	email varchar(50) NOT NULL UNIQUE,
+	password varchar(255) NOT NULL
 );
 
-TRUNCATE TABLE files;
+CREATE TABLE IF NOT EXISTS files (
+	id BIGINT UNSIGNED PRIMARY KEY NOT NULL,
+	user_id BIGINT UNSIGNED NOT NULL,
+	name varchar(200) NOT NULL,
+	location varchar(200) NOT NULL,
+	f_type varchar(200) NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+INSERT INTO users (id, email, password) VALUES
+		(1, 'abc@gmail.com', "0f4dd6c67bc8c827a2b181bc763f9ab96166d8f50840fe1ae0bbc0e77464da2c"),
+		(2, 'john@gmail.com', "d9ede7c2ece01b059ec3af2f8b47836a6feda626cf0a76e49c6c9be2b151456d");
 
 DELIMITER $$
 DROP PROCEDURE IF EXISTS insert_if_empty;
@@ -19,9 +31,9 @@ BEGIN
 	SELECT COUNT(*) INTO row_count FROM files;
 
 	IF row_count = 0 THEN	
-		INSERT INTO files (id, name, location, f_type) VALUES
-		(1, 'file.txt', '/data/fm_files/file.txt', 'txt'),
-		(2, 'users.json', '/data/fm_files/users.json', 'json');
+		INSERT INTO files (id, name, location, f_type, user_id) VALUES
+		(1, 'file.txt', '/data/fm_files/file.txt', 'txt', 1),
+		(2, 'users.json', '/data/fm_files/users.json', 'json', 2);
 	END IF;
 END $$
 
